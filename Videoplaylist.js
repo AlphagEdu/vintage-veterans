94d7a4c777a1c85a7a648e59699b80471c1f7add
AlphagEdu <alphag.edu@gmail.com>
2024-08-13 08:21:24 +0530

    Videoplaylist.js

--- Videoplaylist.js
+++ Videoplaylist.js
@@ -1,55 +1,25 @@
 const videosList = [
-{
-	video: '0812.mp4',
-	title: 'Exercise-1'
-},
-{
-	video: '0810.mp4',
-	title: 'Exercise-2'
-},
-{
-	video: '0811.mp4',
-	title: 'Exercise-3'
-},
-{
-	video: '0809.mp4',
-	title: 'Exercise-4'
-},
-{
-	video: '0813.mp4',
-	title: 'Exercise-5'
-},
-{
-	video: '0814.mp4',
-	title: 'Exercise-6'
-},
-{
-	video: '0815.mp4',
-	title: 'Exercise-7'
-},
-{
-	video: '0809.mp4',
-	title: 'Exercise-8'
-},
-{
-	video: '0890.mp4',
-	title: 'Exercise-9'
-},
-]
+	{ video: '0812.mp4', title: 'Exercise-1' },
+	{ video: '0810.mp4', title: 'Exercise-2' },
+	{ video: '0811.mp4', title: 'Exercise-3' },
+	{ video: '0809.mp4', title: 'Exercise-4' },
+	{ video: '0813.mp4', title: 'Exercise-5' },
+	{ video: '0814.mp4', title: 'Exercise-6' },
+	{ video: '0815.mp4', title: 'Exercise-7' },
+	{ video: '0809.mp4', title: 'Exercise-8' },
+	{ video: '0890.mp4', title: 'Exercise-9' }
+];
 
-const categories = [...new Set(videosList.map((item) => { return item }))]
-document.getElementById('videosList').innerHTML = categories.map((item) => {
+document.getElementById('videosList').innerHTML = videosList.map((item) => {
 	var { video, title } = item;
-	return (
-		<div class="list active">
-		<video src=${video} class="list-video"></video>
-		<h3 class="list-title">${title}</h3>
-		</div>
-		)
-}).join('')
+	return `
+		<div class="list">
+			<video src="${video}" class="list-video"></video>
+			<h3 class="list-title">${title}</h3>
+		</div>`;
+}).join('');
 
 let videoList = document.querySelectorAll('.video-list-container .list');
-videoList.forEach(remove => { remove.classList.remove('active') });
 videoList.forEach(vid => {
 	vid.onclick = () => {
 		videoList.forEach(remove => { remove.classList.remove('active') });
